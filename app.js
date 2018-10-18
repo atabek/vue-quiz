4e6e56bf291cba30f77010c21151ca3f0830bc82
Excellent <excellent.nomad@gmail.com>
2018-10-18 12:15:09 +0600

    added response tracking

--- app.js
+++ app.js
@@ -3,24 +3,27 @@ const test = {
     questions: [
         {
             text: "Who is the strongest?",
+            order: 1,
             answers: [
-                "Superman",
+                "Superman - right answer",
                 "The Terminator",
                 "Waluigi, obviously"
             ]
         },
         {
             text: "What is the best site ever created?",
+            order: 2,
             answers: [
-                "SitePoint",
+                "SitePoint - right answer",
                 "Simple Steps Code",
                 "Trick question; they're both the best"
             ]
         },
         {
             text: "Where is Waldo really?",
+            order: 3,
             answers: [
-                "Antarctica",
+                "Antarctica - right answer",
                 "Exploring the Pacific Ocean",
                 "Sitting in a tree",
                 "Minding his own business, so stop asking"
@@ -38,11 +41,13 @@ new Vue({
         testSize: 0,
         nextDisabled: false,
         prevDisabled: true,
+        responses: null
     },
     methods: {
         startTest: function () {
             this.testIsRunning = true;
             this.testSize = this.test.questions.length;
+            this.responses = new Array(this.testSize);
         },
         next: function () {
             this.index++;
@@ -55,14 +60,32 @@ new Vue({
             if (!this.prevDisabled) { this.nextDisabled = false; }
         },
         getCurrentQuestion: function() { return this.test.questions[this.index]; },
+        getRightAnswerForCurrentQuestion: function() {
+            let currentQuestion = this.test.questions[this.index];
+            return currentQuestion.answers[0];
+        },
         chooseAnswer: function(answer) {
-            console.log(answer + " selected");
+            console.log(answer, " selected");
+            let rightAnswer = this.getRightAnswerForCurrentQuestion();
+            if (rightAnswer === answer) {
+                this.responses[this.index] = true;
+                console.log("Right answer selected");
+                console.log(this.responses);
+            } else {
+                this.responses[this.index] = false;
+                console.log(this.responses);
+            }
+        },
+        answerSelected: function() {
+            return true;
         }
     },
     computed: {
         currentQuestionText: function() { return this.getCurrentQuestion().text; },
         currentQuestionAnswers: function() {
-            return this.getCurrentQuestion().answers.sort(function() { return 0.5 - Math.random() });
+            const answers = Object.assign([], this.getCurrentQuestion().answers);
+            const randomizedAnswers = answers.sort(function() { return 0.5 - Math.random() });
+            return randomizedAnswers;
         }
     }
 });
